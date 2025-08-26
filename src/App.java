0c8c25ee8626f24c2c9129362d478e41ec6eadd7
Andrewv41 <andrew.varanese@gilmour.org>
2025-08-26 13:15:55 +0000

    Teusday class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Data types
+        int x = 5;
+        char c = 'C';
+        String one = "hello";
+        double z = 2.098;
+        
+        int y;//decleration
+        y = 72;//initilization
+        int infractedStudents;
+        //camelCasing
+        final int CONNY = 5;
+        //constants are all caps
+        System.out.println(x-3);
+        x = x - 3;
+        x -= 3;//cooler way of writig ^
+        System.out.println(x);
+        //comparisons
+        boolean b = true;
+        System.out.println(b);
+        System.out.println(!b);
+        //Conditional
+        if(!b) System.out.println("b Passed");
+        // = is setting a variable, while == is comparing the values
+
+        //Short circuit Evaluation : when the system skips running the second half of the eveluation because the conclusion is reached by only reading the first line.
+        int divisor = 0;
+        if(divisor != 0 && 5/divisor >1) System.out.println("Oh no!");
+
     }
 }
